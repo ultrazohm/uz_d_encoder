d70c7e2e7f1cc12f9068bffb5470d345fe059db9
EykeLiegmann <eyke.liegmann@tum.de>
2021-07-13 13:41:14 +0200

    test run

--- IP-Core/Resolver_fix/resolver.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
+++ IP-Core/Resolver_fix/resolver.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2020.1.1 (win64) Build 2960000 Wed Aug  5 22:57:20 MDT 2020
---Date        : Tue Jul 13 13:15:17 2021
+--Date        : Tue Jul 13 13:33:51 2021
 --Host        : TUEIEAL-TM01 running 64-bit major release  (build 9200)
 --Command     : generate_target design_1_wrapper.bd
 --Design      : design_1_wrapper
